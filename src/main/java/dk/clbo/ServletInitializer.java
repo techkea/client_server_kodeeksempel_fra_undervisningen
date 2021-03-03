416dc024978be7826465336952a7c3426c881c45
clbokea <clbo@kea.dk>
2021-03-03 13:02:40 +0100

    initial

---
+++ src/main/java/dk/clbo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package dk.clbo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClboApplication.class);
+    }
+
+}
